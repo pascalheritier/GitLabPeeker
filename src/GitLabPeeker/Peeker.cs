3c0f309e7452f0c496668ac5a17c0233d88bc51e
laurentgobet-Adonite <129828228+laurentgobet-Adonite@users.noreply.github.com>
2025-04-15 11:23:35 +0200

    Add support of multiple pipeline status (#1)

--- src/GitLabPeeker/Peeker.cs
+++ src/GitLabPeeker/Peeker.cs
@@ -47,31 +47,51 @@ internal class Peeker
                 List<Project> peekingProjects = new();
                 foreach (Group group in peekingGroups)
                     peekingProjects.AddRange(await client.Groups.GetProjectsAsync(group.Id));
-                Dictionary<Project, IEnumerable<Pipeline>> runningProjectPipelines = new();
+                Dictionary<PipelineStatus, Dictionary<Project, IEnumerable<Pipeline>>> projectPipelines = new();
 
                 while (!stopAsked)
                 {
                     DateTime start = DateTime.UtcNow;
-                    runningProjectPipelines.Clear();
-                    string logMessage = string.Empty;
+                    foreach (Dictionary<Project, IEnumerable<Pipeline>> projectPipelinesValue in projectPipelines.Values)
+                    {
+                        projectPipelinesValue.Clear();
+                    }
 
                     // get pipelines info
                     foreach (Project project in peekingProjects)
                     {
-                        IList<Pipeline> runningPipelines = await client.Pipelines.GetAsync(project, (option) => option.Status = PipelineStatus.Running);
-                        if (runningPipelines.Count > 0)
-                            runningProjectPipelines[project] = runningPipelines;
+                        foreach (PipelineStatus pipelineStatus in _appConfiguration.StatusToPeek)
+                        {
+                            IList<Pipeline> pipelines = await client.Pipelines.GetAsync(project, (option) => option.Status = pipelineStatus);
+                            if (pipelines.Count > 0)
+                            {
+                                if (!projectPipelines.ContainsKey(pipelineStatus))
+                                    projectPipelines[pipelineStatus] = new Dictionary<Project, IEnumerable<Pipeline>>();
+                                projectPipelines[pipelineStatus][project] = pipelines;
+
+                            }
+                        }
                     }
 
                     // print
-                    logMessage += $"Total running pipelines (peeking group '{_appConfiguration.GroupPeeking}'): {runningProjectPipelines.Values.SelectMany(pipeline => pipeline).Count()}";
-                    if (runningProjectPipelines.Count > 0)
+                    foreach (PipelineStatus pipelineStatus in _appConfiguration.StatusToPeek)
                     {
-                        logMessage += " | Projects: ";
-                        logMessage += string.Join(", ", runningProjectPipelines.Keys.Select(project => $"{project.Name} ({runningProjectPipelines[project].Count()})"));
+                        if (!projectPipelines.TryGetValue(pipelineStatus, out Dictionary<Project, IEnumerable<Pipeline>>? projectPipeline))
+                        {
+                            _logger.LogInformation($"No {pipelineStatus} pipeline (peeking group '{_appConfiguration.GroupPeeking}')");
+                            continue;
+                        }
+
+                        string logMessage = $"Total {pipelineStatus} pipelines (peeking group '{_appConfiguration.GroupPeeking}'): {projectPipeline.Values.SelectMany(pipeline => pipeline).Count()}";
+                        if (projectPipeline.Count > 0)
+                        {
+                            logMessage += " | Projects: ";
+                            logMessage += string.Join(", ", projectPipeline.Keys.Select(project => $"{project.Name} ({projectPipeline[project].Count()})"));
+                        }
+
+                        _logger.LogInformation(logMessage);
                     }
-                    _logger.LogInformation(logMessage);
-                    
+
                     // wait refresh rate
                     while (DateTime.UtcNow.AddSeconds(_appConfiguration.MinRefreshRateSeconds) < start)
                         Thread.Sleep(10);
